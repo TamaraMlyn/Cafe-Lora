b25f040635590f07cef517597996e59b15ebd170
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-05-01 17:11:31 +0200

    Drink component and ordering is working

--- src/Drinks/index.js
+++ src/Drinks/index.js
@@ -8,21 +8,35 @@ export const Drink = (props) => {
     <div class="drink__product">
       <div class="drink__cup">
           <img src="/assets/cups/${props.id}.png" />
-      </div>
-      <div class="drink__info">
+          </div>
+          <div class="drink__info">
           <h3>${props.name}</h3>
-      </div>
-    </div>
-    <div class="drink__controls">
-      <button class="order-btn">Objednat</button>
-    </div>
-  `;
+          </div>
+          </div>
+          <div class="drink__controls">
+          <button class="order-btn">Objednat</button>
+          </div>
+          `;
 
   const infoElm = drinkElm.querySelector('.drink__info');
-
   props.layers.forEach((layer) => {
     infoElm.innerHTML += Layer(layer);
   });
+  // 4 - objednávání nápoje
 
+  const orderBtnElm = drinkElm.querySelector('.order-btn');
+  const drinkCupElm = drinkElm.querySelector('.drink__cup');
+
+  const shortOrder = () => {
+    drinkCupElm.classList.toggle('drink__cup--selected');
+    if (!props.ordered) {
+      orderBtnElm.textContent = 'Zrušit';
+    } else {
+      orderBtnElm.textContent = 'Objednat';
+    }
+    props.ordered = !props.ordered;
+  };
+
+  orderBtnElm.addEventListener('click', shortOrder);
   return drinkElm;
 };
--- src/index.js
+++ src/index.js
@@ -21,36 +21,6 @@ for (let i = 0; i < navAllElm.length; i++) {
   navAllElm[i].addEventListener('click', navClosed);
 }
 
-// 4 - objednávání nápoje
-
-// const orderBtnElm = document.querySelector('.order-btn');
-// const drinkCupElm = document.querySelector('.drink__cup');
-// let ordered = false;
-
-// // const order = () => {
-// //   if (ordered === false) {
-// //     drinkCupElm.classList.add('drink__cup--selected');
-// //     orderBtnElm.textContent = 'Zrušit';
-// //     ordered = true;
-// //   } else {
-// //     drinkCupElm.classList.remove('drink__cup--selected');
-// //     orderBtnElm.textContent = 'Objednat';
-// //     ordered = false;
-// //   }
-// // };
-
-// const shortOrder = () => {
-//   drinkCupElm.classList.toggle('drink__cup--selected');
-//   if (ordered === false) {
-//     orderBtnElm.textContent = 'Zrušit';
-//   } else {
-//     orderBtnElm.textContent = 'Objednat';
-//   }
-//   ordered = !ordered;
-// };
-
-// orderBtnElm.addEventListener('click', shortOrder);
-
 // 5 - ingredience jako komponenty
 
 const layerElm = document.querySelector('.drink__info');
