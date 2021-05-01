45a24813e68b32a0ad52c0125a826ae2e0c6b3ff
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-05-01 12:11:55 +0200

    JS page navigation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navElm = document.querySelector('nav');
+const navBtnElm = document.querySelector('#nav-btn');
+
+navBtnElm.addEventListener('click', () =>
+  navElm.classList.toggle('nav-closed'),
+);
+
+const navAllElm = navElm.querySelectorAll('a');
+
+const navClosed = () => {
+  navElm.classList.add('nav-closed');
+};
+
+for (let i = 0; i < navAllElm.length; i++) {
+  navAllElm[i].addEventListener('click', navClosed);
+}
