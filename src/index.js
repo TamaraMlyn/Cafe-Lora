1acc8abe4b961084af34fadde2a50ead3c3f406f
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-05-01 12:33:47 +0200

    Order btn works

--- src/index.js
+++ src/index.js
@@ -18,3 +18,22 @@ const navClosed = () => {
 for (let i = 0; i < navAllElm.length; i++) {
   navAllElm[i].addEventListener('click', navClosed);
 }
+
+// 4 - objednávání nápoje
+
+const orderBtnElm = document.querySelector('.order-btn');
+let ordered = false;
+
+const order = () => {
+  if (ordered === false) {
+    orderBtnElm.classList.add('drink__cup', 'drink__cup--selected');
+    orderBtnElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    orderBtnElm.classList.remove('drink__cup', 'drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
+};
+
+orderBtnElm.addEventListener('click', order);
